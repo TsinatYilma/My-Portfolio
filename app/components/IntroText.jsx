09b40522d820ec42d4f52679206871b1e5d42bc0
Tsinat <tsinatyilma16@gmail.com>
2025-10-21 18:30:52 +0300

    6 grind on the gsoa

---
+++ app/components/IntroText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import gsap from 'gsap/all'
+import { useGSAP } from '@gsap/react'
+
+const IntroText = () => {
+    useGSAP(()=>{
+        
+    })
+  return (
+    <div>IntroText</div>
+  )
+}
+
+export default IntroText
